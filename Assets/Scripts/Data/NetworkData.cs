5726b44bd76dc066da8e05137e604710c35c2aec
Gunjita-Jamwal <gunjita.jamwal@gmail.com>
2018-09-21 16:20:18 +0530

    network data class created

---
+++ Assets/Scripts/Data/NetworkData.cs
@@ -0,0 +1,14 @@
+﻿using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VesselEncounter
+{
+
+    public class NetworkData : SingletonMonoBehaviour<NetworkData>
+    {
+        public List<Region> regions;
+
+    }
+}
