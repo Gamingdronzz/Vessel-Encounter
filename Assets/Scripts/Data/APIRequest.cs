4444772340e8cdc8fbcf2a277ff4db4e8d819be6
Gunjita-Jamwal <gunjita.jamwal@gmail.com>
2018-09-24 22:52:46 +0530

    class for api request and response

---
+++ Assets/Scripts/Data/APIRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEngine;
+
+namespace VesselEncounter { 
+
+public class APIRequest :SingletonMonoBehaviour<APIRequest> {
+
+        private const string API_BASE = "";
+        private string api;
+
+        public void setAPI(string end) {
+            api = API_BASE + end;
+        }
+
+        public string makeRequest()
+        {
+            HttpWebRequest request =(HttpWebRequest)WebRequest.Create(api);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string jsonResponse = reader.ReadToEnd();
+            return jsonResponse;
+        }
+    }
+}
